e045038f9bfc2c4562a32e7a25aa3bbd76008396
brenthaertlein <brenthaertlein@gmail.com>
2017-11-18 23:45:14 -0500

    feature/FOG-3
    * ItemRepository javadoc & annotation

--- src/main/java/com/nodemules/games/forest/orm/repository/ItemRepository.java
+++ src/main/java/com/nodemules/games/forest/orm/repository/ItemRepository.java
@@ -2,8 +2,15 @@ package com.nodemules.games.forest.orm.repository;
 
 import com.nodemules.games.forest.orm.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * @author brent
+ * @since 11/18/17.
+ */
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
   Item findByName(String name);
+
 }
